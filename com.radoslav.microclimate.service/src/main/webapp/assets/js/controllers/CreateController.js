e96484e29fa91c067749213e68e2f636e428fcdf
RaISug <radoslav.i.sugarev@gmail.com>
2016-01-08 22:23:35 +0200

    Implement creation controller
    Change-Id: Ief4b8f8be2e5a8f5de3feb72ac9fc75bb0cbed32
    Signed-off-by: RaISug <radoslav.i.sugarev@gmail.com>

--- com.radoslav.microclimate.service/src/main/webapp/assets/js/controllers/CreateController.js
+++ com.radoslav.microclimate.service/src/main/webapp/assets/js/controllers/CreateController.js
@@ -2,7 +2,7 @@
 	
 	var module = angular.module("ApplicationController");
 	
-	var CreateController = function($scope) {
+	var CreateController = function($scope, RestUtil, Destinations) {
 		$scope.buttonText = "Добави";
 		$scope.googleMap = {
 			center: {
@@ -11,8 +11,66 @@
 			},
 			zoom: 6
 		};
+		
+		// ==== Define model functions ====
+		$scope.executeRequest = jQuery.proxy(executeBackendRequest, $scope, RestUtil, Destinations);
 	};
 	
-	module.controller("CreateController", ["$scope", CreateController]);
+	module.controller("CreateController", ["$scope", "RestUtil", "Destinations", CreateController]);
+	
+	var executeBackendRequest = function(RestUtil, Destinations, oData) {
+		var requestData = prepareRequestData(oData);
+		requestData.url = Destinations.getStatisticCreationEndpoint();
+		
+		RestUtil.POST(requestData, onSuccess, onError);
+	};
+	
+	var prepareRequestData = function(oData) {
+		var data = {
+			temperature: oData.temperature,
+			rainfall: oData.rainfall,
+			humidity: oData.humidity,
+			snowCover: oData.snowCover,
+			windSpeed: oData.windSpeed,
+			latitude: oData.latitude,
+			longitude: oData.longitude,
+            date: oData.date
+		};
+		
+		return {
+			method: "POST",
+			headers: "application/json",
+			data: JSON.stringify(data)
+		};
+	};
+	
+	var onSuccess = function(xhrResponse) {
+		this.title = "Успешен запис";
+		this.text = "Благодаря ви за отделеното време, данните които" +
+						" предоставихте бяха успешно записани. Моля " +
+						"изчакайте докато страницата бъде презаредена";
+		
+		$("#request-execution-result-dialog").modal({
+			backdrop: "static"
+		});
+		
+		setTimeout(function() {
+			location.reload();
+		}, 1500);
+	};
+	
+	var onError = function(xhrResponse) {
+		this.title = "Неуспешен запис";
+		this.text = "Данните не бяха успешно записани." +
+						"Статус на грешката: [" + 
+							xhrResponse.status
+						+ "], хвърлена грешка: [" + 
+							xhrResponse.statusText
+						+ "].Информация от сървъра: [" + 
+							(typeof xhrResponse.headers()["X-Request-Result"] === "undefined" ? "Няма информация" : xhrResponse.headers()["X-Request-Result"])
+						+ "]";
+		
+		$("#request-execution-result-dialog").modal({ keyboard: true });
+	};
 	
 })();
