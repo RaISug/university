b1fb1db75f3cd7222f040d1178c93141c5351103
RaISug <radoslav.i.sugarev@gmail.com>
2016-01-09 12:19:38 +0200

    Implement delete entry functionallity
    Change-Id: I29c8262afe27d20451ee48def8a502446f82819f
    Signed-off-by: RaISug <radoslav.i.sugarev@gmail.com>

--- com.radoslav.microclimate.service/src/main/webapp/assets/js/controllers/DeleteController.js
+++ com.radoslav.microclimate.service/src/main/webapp/assets/js/controllers/DeleteController.js
@@ -2,10 +2,48 @@
 
 	var module = angular.module("ApplicationController");
 	
-	var DeleteController = function($scope) {
-
+	var DeleteController = function($scope, $routeParams, RestUtil, Destinations) {
+		$scope.executeRequest = jQuery.proxy(executeBackendRequest, $scope, $routeParams, RESTUtil, DestinationUtil);
 	};
 	
-	module.controller("DeleteController", ["$scope", DeleteController]);
+	module.controller("DeleteController", ["$scope", "$routeParams", "RestUtil", "Destinations", DeleteController]);
+	
+	var executeBackendRequest = function($routeParams, RestUtil, Destinations) {
+		var requestData = prepareRequestData($routeParams, Destinations);
+		RestUtil.DELETE(requestData, jQuery.proxy(onSuccess, this), jQuery.proxy(onError, this));
+	};
+	
+	var prepareRequestData = function($routeParams, Destinations) {
+		var entryId = routeParams.id;
+		
+		return {
+			method : "DELETE",
+			url : String.format(Destinations.getStatisticDeleteEdnpoint(), entryId)
+		};
+	};
+	
+	var onSuccess = function(xhrResponse) {
+		this.title = "Успешно изтрити данни.";
+		this.resultMessage = "Успешно изтрити данни. Моля изчакайте страницата да бъде презаредена.";
+		
+		$('#delete-products-result-modal').modal({
+			backdrop: "static"
+		});
+		
+		setTimeout(function() {
+			location.hash = "#/delete";
+		}, 1500);
+	};
+	
+	var onError = function(xhrResponse) {
+		this.title = "Неуспешно изтриване.";
+		this.resultMessage = "Данните не бяха изтрити." +
+			"Статус на грешката: [" + xhrResponse.status + "], хвърлена грешка: [" + xhrResponse.statusText + "]." +
+			"Информация от сървъра: [" 
+				+ (typeof xhrResponse.headers()["X-Request-Result"] === "undefined" ? "Няма информация" : xhrResponse.headers()["X-Request-Result"]) 
+			+ "]";
+		
+		$('#delete-products-result-modal').modal({ keyboard: true });
+	};
 	
 })();
